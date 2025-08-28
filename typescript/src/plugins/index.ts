636bd1ff921bb79df69319a85979d356339fb9ad
Jake Hall <jake.hall@hashgraph.com>
2025-08-28 15:44:24 +0800

    chore: add backwards compatability for old export names
    Signed-off-by: Jake Hall <jake.hall@hashgraph.com>

--- typescript/src/plugins/index.ts
+++ typescript/src/plugins/index.ts
@@ -16,3 +16,24 @@ export {
   coreEVMPlugin,
   coreEVMPluginToolNames,
 };
+
+// Deprecated exports for backward compatibility
+/**
+ * @deprecated Use coreTokenPlugin instead. This export will be removed in a future version.
+ */
+export const coreHTSPlugin = coreTokenPlugin;
+
+/**
+ * @deprecated Use coreTokenPluginToolNames instead. This export will be removed in a future version.
+ */
+export const coreHTSPluginToolNames = coreTokenPluginToolNames;
+
+/**
+ * @deprecated Use coreEVMPlugin instead. This export will be removed in a future version.
+ */
+export const coreSCSPlugin = coreEVMPlugin;
+
+/**
+ * @deprecated Use coreEVMPluginToolNames instead. This export will be removed in a future version.
+ */
+export const coreSCSPluginToolNames = coreEVMPluginToolNames;
