2fab305a75f41ad730b72c1de092cdf9c650b9bb
skurzyp-blockydevs <stanislaw.kurzyp@blockydevs.com>
2026-04-09 09:35:02 +0200

    docs: add warning for default key usage in create account tool
    Signed-off-by: skurzyp-blockydevs <stanislaw.kurzyp@blockydevs.com>

--- typescript/src/plugins/core-account-plugin/tools/account/create-account.ts
+++ typescript/src/plugins/core-account-plugin/tools/account/create-account.ts
@@ -23,6 +23,7 @@ IMPORTANT: All parameters are optional. If the user does not explicitly provide 
 
 NOTE: On Hedera, multiple accounts can be created and controlled using the same key pair (e.g., the operator account's key). This means that assets on the newly created account will be accessible using the same key pair as the operator account if no explicit public key is provided.
 CRITICAL: Do NOT tell the user that you have generated a new key pair. This tool CANNOT generate a new key pair.
+WARNING: If no public key is provided, the operator's key will be used. 
 
 Parameters:
 - publicKey (string, optional): Public key to use for the account. If not provided, the tool will use the operator's public key.
